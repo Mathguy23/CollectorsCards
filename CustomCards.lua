b381f6cdef76c78c8dce2ea32f43d365321b38e8
Mathguy23 <165744246+Mathguy23@users.noreply.github.com>
2025-04-05 20:55:33 -0400

    version

--- CustomCards.lua
+++ CustomCards.lua
@@ -4,7 +4,7 @@
 --- PREFIX: pc
 --- MOD_AUTHOR: [mathguy]
 --- MOD_DESCRIPTION: Playing Cards with special abilities.
---- VERSION: 1.0.0
+--- VERSION: 1.0.1
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
