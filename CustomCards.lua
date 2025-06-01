de6c904dac1f239af101ac419839d18102ab8ade
Mathguy23 <165744246+Mathguy23@users.noreply.github.com>
2025-06-01 19:52:53 -0400

    deck of jokes things

--- CustomCards.lua
+++ CustomCards.lua
@@ -4,7 +4,7 @@
 --- PREFIX: pc
 --- MOD_AUTHOR: [mathguy]
 --- MOD_DESCRIPTION: Playing Cards with special abilities.
---- VERSION: 1.0.9a
+--- VERSION: 1.0.9b
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
